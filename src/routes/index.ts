667b2b330d07bb998a9594a6fedc252bd812c9ae
oigabrielteodoro <oi@gabrielteodoro.com>
2021-04-20 14:59:39 -0300

    feat: create settings controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,21 +1,11 @@
 import { Router } from 'express';
 
-import { getCustomRepository } from 'typeorm';
-
-import { SettingsRepositories } from '../repositories/SettingsRepository';
+import { SettingsController } from '../controllers/SettingsController';
 
 const routes = Router();
 
-routes.post('/settings', async (request, response) => {
-  const { body } = request;
+const settingsController = new SettingsController();
 
-  const settingsRepository = getCustomRepository(SettingsRepositories);
-
-  const settings = settingsRepository.create(body);
-
-  await settingsRepository.save(settings);
-
-  return response.json(settings);
-})
+routes.post('/settings', settingsController.create);
 
 export { routes };
