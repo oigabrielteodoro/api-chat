9ea7eb05954cfafd16e1a1918bd20d90fd55e2ae
oigabrielteodoro <oi@gabrielteodoro.com>
2021-04-20 14:22:01 -0300

    feat: Initial structure

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (request, response) => {
+  return response.json({
+    message: 'Olá NLW #5!'
+  })
+})
+
 app.listen(3333, () => console.log('Server is running on port 3333'));
