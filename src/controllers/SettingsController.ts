c4e0fa8ddf1814576dbd9ce8f30c5811713c0084
oigabrielteodoro <oi@gabrielteodoro.com>
2021-04-24 19:57:43 -0300

    feat: created SettingsService to all features

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -2,16 +2,15 @@ import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 
 import { SettingsRepositories } from "../repositories/SettingsRepository";
+import { SettingsService } from "../services/SettingsService";
 
 class SettingsController {
   async create(request: Request, response: Response): Promise<Response> {
     const { body } = request;
 
-    const settingsRepository = getCustomRepository(SettingsRepositories);
+    const settingsService = new SettingsService();
 
-    const settings = settingsRepository.create(body);
-  
-    await settingsRepository.save(settings);
+    const settings = await settingsService.create(body);
 
     return response.json(settings);
   }
---
+++ src/services/SettingsService.ts
@@ -0,0 +1,35 @@
+import { getCustomRepository } from "typeorm";
+
+import { Setting } from "../entities/Setting";
+
+import { SettingsRepositories } from "../repositories/SettingsRepository";
+
+interface ISettingsCreate {
+  chat: boolean;
+  username: string;
+}
+
+class SettingsService {
+  async create({ chat, username }: ISettingsCreate): Promise<Setting> {
+    const settingsRepository = getCustomRepository(SettingsRepositories);
+
+    const userAlreadyExists = await settingsRepository.findOne({
+      username,
+    });
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists');
+    }
+
+    const settings = settingsRepository.create({
+      chat,
+      username,
+    });
+  
+    await settingsRepository.save(settings);
+
+    return settings;
+  }
+}
+
+export { SettingsService };
