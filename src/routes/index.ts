e7846bb2d090fa2480ea525c74be6cb1463bd7a6
oigabrielteodoro <oi@gabrielteodoro.com>
2021-04-24 22:04:34 -0300

    feat: added craete user route in routes application

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
+import { UsersController } from '../controllers/UsersController';
 import { SettingsController } from '../controllers/SettingsController';
 
 const routes = Router();
 
+const usersController = new UsersController();
 const settingsController = new SettingsController();
 
+routes.post('/users', usersController.create);
 routes.post('/settings', settingsController.create);
 
 export { routes };
