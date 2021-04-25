b04f2133040f200aa06700e29f7e75b0a1a837f0
oigabrielteodoro <oi@gabrielteodoro.com>
2021-04-24 22:55:10 -0300

    feat: created migration to create connections table

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,5 @@
-import express from 'express';
+import { http } from './http';
 
-import path from 'path';
-
-import { createServer } from 'http';
-import { Server, Socket } from 'socket.io';
-
-import { routes } from './routes';
-
-import './database';
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, '..', 'public')));
-app.set('views', path.join(__dirname, '..', 'public'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
-app.get('/pages/client', (request, response) => {
-  return response.render('html/client.html');
-});
-
-const http = createServer(app);
-const io = new Server(http);
-
-io.on('connection', (socket: Socket) => {
-  console.log('Se conectou', socket.id);
-});
-
-app.use(express.json());
-app.use(routes);
+import './websocket/client';
 
 http.listen(3333, () => console.log('Server is running on port 3333'));
